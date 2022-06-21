ed9aff68a90d28888428931f472887ff05d0fa99
Draco <diliana.bodurova@abv.bg>
2022-06-21 23:15:41 +0300

    Exam preparation

---
+++ src/Advance/StacksAndQueues01ReverseNumbersWithAStack.java
@@ -0,0 +1,10 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class StacksAndQueues01ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
