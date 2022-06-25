6e71e9868f372bf0942bd5e3014b1af3846f1a3f
Draco <diliana.bodurova@abv.bg>
2022-06-25 20:18:15 +0300

    25/06/22

---
+++ src/Basic/ConcatenateData.java
@@ -0,0 +1,16 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class ConcatenateData {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String lastName = sc.nextLine();
+        int age = Integer.parseInt(sc.nextLine());
+        String city = sc.nextLine();
+        System.out.printf("You are %s %s, a %d-years old person from %s.", name, lastName, age, city);
+    }
+}
+//    Напишете програма, която прочита от конзолата име, фамилия, възраст и град
+//    и печата следното съобщение: "You are <firstName> <lastName>, a <age>-years old person from <town>."
