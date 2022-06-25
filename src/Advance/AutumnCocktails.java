688475948b1da4257833d6f57877e2311662ba2e
Draco <diliana.bodurova@abv.bg>
2022-06-25 19:57:12 +0300

    25/06/22

---
+++ src/Advance/AutumnCocktails.java
@@ -0,0 +1,10 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class AutumnCocktails {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
---
+++ src/Advance/draft.java
@@ -0,0 +1,9 @@
+package Advance;
+
+import java.util.Scanner;
+
+public class draft {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
