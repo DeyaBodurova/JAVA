09676cc2114501d6c588b49a6082e6e4972a57a7
Draco <diliana.bodurova@abv.bg>
2022-06-25 20:07:01 +0300

    25/06/22

--- src/Basic/Num1To10.java
+++ src/Basic/Num1To10.java
@@ -1,4 +1,11 @@
 package Basic;
 
 public class Num1To10 {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 10) {
+            System.out.println(n);
+            n++;
+        }
+    }
 }
