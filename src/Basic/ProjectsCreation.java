1ce27c2d08fc18879bbcc51b543c4079c171f6ae
Draco <diliana.bodurova@abv.bg>
2022-06-26 17:39:48 +0300

    exercise basic 26/06/22

---
+++ src/Basic/ProjectsCreation.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int projects = Integer.parseInt(sc.nextLine());
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.",
+                name, projects * 3, projects);
+    }
+}
