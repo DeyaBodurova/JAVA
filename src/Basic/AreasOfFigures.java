61b53652e1d2cb93e1485e27fb1dc54767f76bb3
Draco <diliana.bodurova@abv.bg>
2022-06-27 20:46:05 +0300

    exercise basic 27/06/22

---
+++ src/Basic/AreasOfFigures.java
@@ -0,0 +1,32 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class AreasOfFigures {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String type = sc.nextLine();
+        double area;
+        switch (type) {
+            case "square":
+                double length = Double.parseDouble(sc.nextLine());
+                area = length * length;
+                break;
+            case "rectangle":
+                double lengthA = Double.parseDouble(sc.nextLine());
+                double lengthB = Double.parseDouble(sc.nextLine());
+                area = lengthA * lengthB;
+                break;
+            case "circle":
+                double radius = Double.parseDouble(sc.nextLine());
+                area = Math.PI * (radius * radius);
+                break;
+            case "triangle":
+                double lengthT = Double.parseDouble(sc.nextLine());
+                double height = Double.parseDouble(sc.nextLine());
+                area = (lengthT * height) / 2;
+                break;
+        }
+        System.out.println(area);
+    }
+}
