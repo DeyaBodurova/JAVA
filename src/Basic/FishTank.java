09665504e652685b9731a4a767c995eaa7a2a09d
Draco <diliana.bodurova@abv.bg>
2022-06-27 20:13:44 +0300

    exercise basic 27/06/22

---
+++ src/Basic/FishTank.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class FishTank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = Integer.parseInt(sc.nextLine());
+        int width = Integer.parseInt(sc.nextLine());
+        int height = Integer.parseInt(sc.nextLine());
+        double percent = Double.parseDouble(sc.nextLine());
+        double volume = length * width * height;
+        double volumeLitters = volume * 0.001;
+        double neededLitters = volumeLitters * (1 - (percent * 0.01));
+        System.out.println(neededLitters);
+    }
+}
