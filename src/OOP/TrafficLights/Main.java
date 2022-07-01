293bda9e76f01eb6272b18d2f6bec76d16956a56
Draco <diliana.bodurova@abv.bg>
2022-07-01 22:57:22 +0300

    abstract exercises 01/07/2022

--- src/OOP/TrafficLights/Main.java
+++ src/OOP/TrafficLights/Main.java
@@ -1,4 +1,14 @@
 package OOP.TrafficLights;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> trafficLights = new ArrayList<>();
+        (sc.nextLine().split("\\s+"));
+
+    }
 }
