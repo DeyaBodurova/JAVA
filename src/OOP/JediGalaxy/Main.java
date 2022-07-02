407d2596bcd53f32a5cf22c7780b205e4cbd2939
Draco <diliana.bodurova@abv.bg>
2022-07-02 07:48:44 +0300

    abstract exercises 02/07/2022

--- src/OOP/JediGalaxy/Main.java
+++ src/OOP/JediGalaxy/Main.java
@@ -12,6 +12,16 @@ public class Main {
         int row = coordinates[0];
         int col = coordinates[1];
         int[][] galaxy = new int[row][col];
+        fillTheGalaxy(galaxy);
+    }
 
+    private static void fillTheGalaxy(int[][] matrix) {
+        int n = 0;
+        for (int col = 0; col < matrix.length; col++) {
+            for (int row = 0; row < matrix.length; row++) {
+                matrix[col][row] = n;
+                n++;
+            }
+        }
     }
 }
