89b739f50ae6e59dec2f00ecc10f6665a40ecaf2
Draco <diliana.bodurova@abv.bg>
2022-07-02 12:10:33 +0300

    abstract exercises 02/07/2022

--- src/OOP/JediGalaxy/Main.java
+++ src/OOP/JediGalaxy/Main.java
@@ -6,31 +6,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] coordinates = Arrays.stream(sc.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] coordinates = readPosition(sc.nextLine());
         int row = coordinates[0];
         int col = coordinates[1];
-        int[][] galaxy = new int[row][col];
-        fillTheGalaxy(galaxy);
-        String input = sc.nextLine();
+        BattleField battleField = new BattleField(row, col);
+        Galaxy galaxy = new Galaxy(battleField);
 
-//        while (!input.equals("Let the Force be with you")) {
-            int[] jediCoordinates = Arrays.stream(input.split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            int jediRow = jediCoordinates[0] - 1;
-            int jediCol = jediCoordinates[1] - 1;
-            int[] evilCoordinates = Arrays.stream(sc.nextLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            int evilRow = evilCoordinates[0] - 1;
-            int evilCol = evilCoordinates[1] - 1;
-            moveEvil(galaxy, evilRow, evilCol);
-        input = sc.nextLine();
-        if (input.equals("Let the Force be with you")) {
-                System.out.println(moveJedi(galaxy, jediRow, jediCol));
-            }
-//        }
+        String command = sc.nextLine();
+        while (!command.equals("Let the Force be with you")) {
+            int[] jediPosition = readPosition(command);
+            int[] evilPosition = readPosition(sc.nextLine());
+            int jediRow = jediPosition[0];
+            int jediCol = jediPosition[1];
+            int evilRow = evilPosition[0];
+            int evilCol = evilPosition[1];
+            galaxy.moveEvil(evilRow, evilCol);
+            long starsCollected = galaxy.moveJedi(jediRow, jediCol);
+            command = sc.nextLine();
+            System.out.println(starsCollected);
+
+        }
+    }
+
+    private static int[] readPosition(String command) {
+        return Arrays.stream(command.split("\\s+")).mapToInt(Integer::parseInt).toArray();
     }
 }
