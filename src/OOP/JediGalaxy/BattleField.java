d8add72d79da7b5ccb18394f8887e84b2cd6ee4d
Draco <diliana.bodurova@abv.bg>
2022-07-02 11:40:49 +0300

    abstract exercises 02/07/2022

--- src/OOP/JediGalaxy/BattleField.java
+++ src/OOP/JediGalaxy/BattleField.java
@@ -1,4 +1,21 @@
 package OOP.JediGalaxy;
 
 public class BattleField {
+    private int[][] battleField;
+
+    public BattleField(int r, int c) {
+        this.battleField = new int[r][c];
+        this.fillInBattleField(r,c);
+    }
+
+    public void fillInBattleField(int r, int c) {
+        int num = 0;
+        for (int row = 0; r < r; r++) {
+            for (int col = 0; col < c; col++) {
+                this.battleField[r][c] = num ++;
+            }
+        }
+    }
+
+
 }
