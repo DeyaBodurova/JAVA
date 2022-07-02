3e1b710332874de3e7522cc1754fde53d6e9d058
Draco <diliana.bodurova@abv.bg>
2022-07-02 07:15:07 +0300

    abstract exercises 02/07/2022

--- src/OOP/TrafficLights/TrafficLight.java
+++ src/OOP/TrafficLights/TrafficLight.java
@@ -10,4 +10,20 @@ public class TrafficLight {
     public Colours getCurrentColour(){
         return currentColour;
     }
+
+    public void changeColour(Colours currentColour) {
+        switch (currentColour){
+            case "RED":
+                this.currentColour = Colours.GREEN;
+                break;
+            case "GREEN":
+                this.currentColour = Colours.YELLOW;
+                break;
+            case "YELLOW":
+                this.currentColour = Colours.RED;
+                break;
+            default:
+                throw new IllegalArgumentException("No such colour.");
+        }
+    }
 }
