a3062134085d67b21dec5306caaafe7d2bc11479
Draco <diliana.bodurova@abv.bg>
2022-07-02 06:59:48 +0300

    abstract exercises 02/07/2022

--- src/OOP/TrafficLights/Main.java
+++ src/OOP/TrafficLights/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<String> trafficLights = new ArrayList<>();
+        trafficLights = sc.nextLine().split("\\s+")
 
     }
 }
