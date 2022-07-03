5bdf5a2b2704acf855052185d04ade21698a644e
Draco <diliana.bodurova@abv.bg>
2022-07-03 09:14:18 +0300

    exercises 03/07/2022

--- src/Advance/StacksAndQueues/MaximumElement.java
+++ src/Advance/StacksAndQueues/MaximumElement.java
@@ -1,9 +1,43 @@
 package Advance.StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaximumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int countCommands = Integer.parseInt(sc.nextLine());
+
+        while(countCommands > 0) {
+            int[] command = Arrays.stream(sc.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            switch (command[0]) {
+                case 1:
+                    stack.push(command[1]);
+                    break;
+                case 2:
+                    stack.pop();
+                    break;
+                case 3:
+                    int maxElement = Integer.MIN_VALUE;
+                    while (!stack.isEmpty()){
+                        if(stack.peek()>maxElement){
+                            maxElement = stack.pop();
+                        } else {
+                            stack.pop();
+                        }
+                    }
+                    System.out.println(maxElement);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid command!");
+            }
+
+            countCommands--;
+        }
+
     }
 }
