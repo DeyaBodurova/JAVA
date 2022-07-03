a720bb48eab01b6f9be072e3ded7d114c07fe491
Draco <diliana.bodurova@abv.bg>
2022-07-03 09:06:42 +0300

    exercises 03/07/2022

---
+++ src/Advance/StacksAndQueues/MaximumElement.java
@@ -0,0 +1,9 @@
+package Advance.StacksAndQueues;
+
+import java.util.Scanner;
+
+public class MaximumElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
