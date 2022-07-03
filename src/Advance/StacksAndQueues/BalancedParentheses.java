b7b9da1d236eb0a962e34dc9d64f0896c058fb16
Draco <diliana.bodurova@abv.bg>
2022-07-03 10:02:55 +0300

    exercises 03/07/2022

---
+++ src/Advance/StacksAndQueues/BalancedParentheses.java
@@ -0,0 +1,9 @@
+package Advance.StacksAndQueues;
+
+import java.util.Scanner;
+
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
