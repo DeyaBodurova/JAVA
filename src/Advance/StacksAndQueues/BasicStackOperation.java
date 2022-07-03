7a1fc924cf12bed77d88cfc30c3843f15ba6d69e
Draco <diliana.bodurova@abv.bg>
2022-07-03 08:58:40 +0300

    exercises 03/07/2022

--- src/Advance/StacksAndQueues/BasicStackOperation.java
+++ src/Advance/StacksAndQueues/BasicStackOperation.java
@@ -12,15 +12,26 @@ public class BasicStackOperation {
         int toPush = data[0];
         int toPop = data[0];
         int checkIfPresent = data[0];
-        List<Integer> numbers = new ArrayList<>();
-        numbers = Arrays.stream(sc.nextLine().split("\\s+"))
+        List<Integer> numbers = Arrays.stream(sc.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
         ArrayDeque<Integer> stack = new ArrayDeque<>();
+
         while (toPush > 0) {
             stack.push(numbers.get(0));
             numbers.remove(0);
             toPush--;
+        }
+
+        while (toPop > 0) {
+            stack.pop();
+            toPop--;
+        }
+
+        if(stack.contains(checkIfPresent)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
     }
 }
