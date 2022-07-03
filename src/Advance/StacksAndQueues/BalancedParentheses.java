d4e884621d9daccd1b7693d289d49e58a659c3e2
Draco <diliana.bodurova@abv.bg>
2022-07-03 10:27:13 +0300

    exercises 03/07/2022

--- src/Advance/StacksAndQueues/BalancedParentheses.java
+++ src/Advance/StacksAndQueues/BalancedParentheses.java
@@ -1,9 +1,64 @@
 package Advance.StacksAndQueues;
-
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class BalancedParentheses {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split("\\s+");
+        if (input.length % 2 != 0) {
+            System.out.println("NO");
+        } else {
+            ArrayDeque<String> stack = new ArrayDeque<>();
+            ArrayDeque<String> queue = new ArrayDeque<>();
+            for (int i = 0; i < input.length / 2; i++) {
+                stack.push(input[i]);
+            }
+            for (int i = input.length / 2; i < input.length ; i++) {
+                queue.add(input[i]);
+            }
+            if(!stack.isEmpty() && !queue.isEmpty()) {
+                while (!stack.isEmpty() && !queue.isEmpty()) {
+                    String current = stack.peek();
+                    switch (current) {
+                        case "{":
+                            if(queue.peek().equals("}")) {
+                                stack.pop();
+                                queue.poll();
+                            } else {
+                                System.out.println("NO");
+                                break;
+                            }
+                            break;
+                        case "[":
+                            if(queue.peek().equals("]")) {
+                                stack.pop();
+                                queue.poll();
+                            } else {
+                                System.out.println("NO");
+                                break;
+                            }
+                            break;
+                        case "(":
+                            if(queue.peek().equals(")")) {
+                                stack.pop();
+                                queue.poll();
+                            } else {
+                                System.out.println("NO");
+                                break;
+                            }
+                            break;
+                        default:
+                            System.out.println("NO");
+                            break;
+                    }
+                }
+                if (stack.isEmpty() && queue.isEmpty()) {
+                    System.out.println("YES");
+                }
+            } else {
+                System.out.println("NO");
+            }
+        }
     }
 }
