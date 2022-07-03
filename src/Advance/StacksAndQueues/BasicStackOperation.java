6734517e2dd7f7ac713c97c690f57c5943c409cf
Draco <diliana.bodurova@abv.bg>
2022-07-03 08:55:12 +0300

    exercises 03/07/2022

--- src/Advance/StacksAndQueues/BasicStackOperation.java
+++ src/Advance/StacksAndQueues/BasicStackOperation.java
@@ -1,9 +1,26 @@
 package Advance.StacksAndQueues;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BasicStackOperation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] data = Arrays.stream(sc.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int toPush = data[0];
+        int toPop = data[0];
+        int checkIfPresent = data[0];
+        List<Integer> numbers = new ArrayList<>();
+        numbers = Arrays.stream(sc.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        while (toPush > 0) {
+            stack.push(numbers.get(0));
+            numbers.remove(0);
+            toPush--;
+        }
     }
 }
