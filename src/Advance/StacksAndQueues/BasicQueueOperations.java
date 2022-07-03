91d92dbc51164a98116d8d7947e8d38008e12705
Draco <diliana.bodurova@abv.bg>
2022-07-03 09:48:49 +0300

    exercises 03/07/2022

---
+++ src/Advance/StacksAndQueues/BasicQueueOperations.java
@@ -0,0 +1,9 @@
+package Advance.StacksAndQueues;
+
+import java.util.Scanner;
+
+public class BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
