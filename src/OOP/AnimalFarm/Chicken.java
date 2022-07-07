3df69bb0f3d599ac25319f2b69cd094b8af6d1b4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-07 21:39:08 +0300

    wip

--- src/OOP/AnimalFarm/Chicken.java
+++ src/OOP/AnimalFarm/Chicken.java
@@ -4,4 +4,20 @@ public class Chicken {
     private String name;
     private int age;
 
+    public Chicken(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        this.age = age;
+    }
+
+    public double productPerDay() {
+
+    }
 }
