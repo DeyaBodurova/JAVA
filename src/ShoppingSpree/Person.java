631aabf0db183f0890fdbede5c75fb1b8ebd0b9b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-08 20:38:16 +0300

    wip

--- src/ShoppingSpree/Person.java
+++ src/ShoppingSpree/Person.java
@@ -1,4 +1,38 @@
 package ShoppingSpree;
 
+import java.util.List;
+
 public class Person {
+
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        this.name = name;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    private void setMoney(double money) {
+        this.money = money;
+    }
+
+   public void buyProduct (Product) {
+
+   }
+
+
+
 }
