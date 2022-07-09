96ff3965b52998db3dddfd49990d4446dd31ee30
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-09 23:12:57 +0300

    wip

--- src/Basic/NumberPyramid.java
+++ src/Basic/NumberPyramid.java
@@ -1,4 +1,22 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class NumberPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int x = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i + j > n) {
+                    break;
+                } else if (i == 0) {
+                    System.out.print(1);
+                }
+                System.out.print(i + j + " ");
+            }
+            System.out.println();
+        }
+    }
 }
