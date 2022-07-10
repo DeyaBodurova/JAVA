723af5bb5cdcd18d64060ef1206cbc619136a352
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-10 23:12:28 +0300

    wip

--- src/Basic/TrainTheTrainers.java
+++ src/Basic/TrainTheTrainers.java
@@ -21,24 +21,10 @@ public class TrainTheTrainers {
             }
             double averageGrade = totalSumGrades / juri;
             allPresentationGrade += averageGrade;
-            System.out.printf("%s - %.2f\n", presentation, averageGrade);
+            System.out.printf("%s - %.2f.\n", presentation, averageGrade);
 
             input = sc.nextLine();
         }
         System.out.printf("Student's final assessment is %.2f.\n", allPresentationGrade /presentationCount);
     }
 }
-//    От конзолата на първият ред се прочита броят на хората в журито n - цяло число в интервала [1…20]
-//
-//        След това на отделен ред се прочита името на презентацията - текст
-//
-//        За всяка една презентация на нов ред се четат n - на брой оценки - реално число в интервала [2.00…6.00]
-//
-//        След изчисляване на средната оценка за конкретна презентация, на конзолата се печата
-//
-//        "{името на презентацията} - {средна оценка}."
-//
-//        След получаване на команда "Finish" на конзолата се печата
-//        "Student's final assessment is {среден успех от всички презентации}." и програмата приключва.
-//
-//        Всички оценки трябва да бъдат форматирани до втория знак след десетичната запетая.
