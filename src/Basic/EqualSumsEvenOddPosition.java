737adeb1a5be0d0d8de63953928ba38b5a740a03
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-10 09:13:15 +0300

    wip

--- src/Basic/EqualSumsEvenOddPosition.java
+++ src/Basic/EqualSumsEvenOddPosition.java
@@ -5,11 +5,25 @@ import java.util.Scanner;
 public class EqualSumsEvenOddPosition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int smaller = Integer.parseInt(sc.nextLine());
+        String[] smaller = (sc.nextLine()).split("");
         int bigger = Integer.parseInt(sc.nextLine());
 
-        for (int i = smaller; i < bigger; i++) {
-            int x = i.;
+        for (int n = Integer.parseInt(String.valueOf(smaller)); n <= bigger ; n++) {
+            int current = Integer.parseInt(String.valueOf(smaller));
+            int sumNumFirst = 0;
+            int sumNumSecond = 0;
+            for (int i = 0; i < smaller.length; i++) {
+                if(i % 2 == 0) {
+                    sumNumFirst += i;
+                } else {
+                    sumNumSecond += i;
+                }
+            }
+            if(sumNumFirst == sumNumSecond) {
+                System.out.print(current + " ");
+            }
+            current++;
         }
+
     }
 }
