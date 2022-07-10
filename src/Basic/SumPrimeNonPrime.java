d4074e0fde0b31b74c85a06efd111a3a5edbfd75
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-10 22:45:55 +0300

    wip

--- src/Basic/SumPrimeNonPrime.java
+++ src/Basic/SumPrimeNonPrime.java
@@ -6,17 +6,17 @@ public class SumPrimeNonPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        int sumPrime =0;
-        int sumNonPrime =0;
+        int sumPrime = 0;
+        int sumNonPrime = 0;
         while (!input.equalsIgnoreCase("stop")) {
-            int num =  Integer.parseInt(input);
-            if (num<0) {
+            int num = Integer.parseInt(input);
+            if (num < 0) {
                 System.out.println("Number is negative");
             } else {
-                if(num%2 == 0 || (num%3==0) || (num%5==0)){
-                    sumNonPrime+=num;
+                if (num % 2 == 0 || (num % 3 == 0) || (num % 5 == 0)) {
+                    sumNonPrime += num;
                 } else {
-                    sumPrime+=num;
+                    sumPrime += num;
                 }
             }
             input = sc.nextLine();
