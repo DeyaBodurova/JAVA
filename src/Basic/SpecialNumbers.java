b2eb5d2f642f9cdaa772585bc0c808c86ca83476
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-11 18:38:18 +0300

    wip

--- src/Basic/SpecialNumbers.java
+++ src/Basic/SpecialNumbers.java
@@ -6,7 +6,10 @@ public class SpecialNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
+        for (int i = 1111; i <= 9999; i++) {
 
+
+        }
 
     }
 }
