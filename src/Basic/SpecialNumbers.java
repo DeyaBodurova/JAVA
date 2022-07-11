dc3fff350f8ef9159653e682be1c4ba6c217e913
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-11 18:35:37 +0300

    wip

--- src/Basic/SpecialNumbers.java
+++ src/Basic/SpecialNumbers.java
@@ -1,7 +1,11 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class SpecialNumbers {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
 
     }
 }
