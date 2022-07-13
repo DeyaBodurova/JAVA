c0dae95b3de6b927ec7e8ba53ac40162dc946ce4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-13 23:14:14 +0300

    wip

--- src/OOP/shoppingSpree/Main.java
+++ src/OOP/shoppingSpree/Main.java
@@ -3,12 +3,11 @@ package OOP.shoppingSpree;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> data = Arrays.stream(sc.nextLine().split("=")).toList();
+        List<String> line1 = Arrays.stream(sc.nextLine().split(";")).toList();
         Person person = new Person(data.get(0), Double.parseDouble(data.get(1)));
         List<String> data2 = Arrays.stream(sc.nextLine().split("=")).toList();
         Person person2 = new Person(data2.get(0), Double.parseDouble(data2.get(1)));
