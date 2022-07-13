f991c96b6c931cc3b0e4b2edb6990997d66e2d26
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-13 23:09:32 +0300

    wip

--- src/OOP/shoppingSpree/Main.java
+++ src/OOP/shoppingSpree/Main.java
@@ -1,7 +1,11 @@
 package OOP.shoppingSpree;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        List<String> data = sc.nextLine().split("=");
     }
 }
