518c30163757fa86adaf78f610c672c6b615c4b5
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-13 23:12:39 +0300

    wip

--- src/OOP/shoppingSpree/Main.java
+++ src/OOP/shoppingSpree/Main.java
@@ -9,5 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<String> data = Arrays.stream(sc.nextLine().split("=")).toList();
+        Person person = new Person(data.get(0), Double.parseDouble(data.get(1)));
+        List<String> data2 = Arrays.stream(sc.nextLine().split("=")).toList();
+        Person person2 = new Person(data2.get(0), Double.parseDouble(data2.get(1)));
+
     }
 }
