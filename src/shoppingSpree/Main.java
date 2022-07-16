a4b355ce58fab40b1445660a37ae6057029689d6
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-16 08:35:46 +0300

    wip

--- src/shoppingSpree/Main.java
+++ src/shoppingSpree/Main.java
@@ -1,9 +1,22 @@
 package shoppingSpree;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Map<String, Double> people = new LinkedHashMap<>();
+        Map<String, Double> products = new LinkedHashMap<>();
+
+        String[] peopleData = sc.nextLine().split(";");
+        for (int i = 0; i < peopleData.length; i++) {
+            String[] currentPerson = peopleData[i].split("=");
+            String currentPersonName = currentPerson[0];
+            double currentPersonMoney = Double.parseDouble(currentPerson[1]);
+            people.put(currentPersonName,currentPersonMoney);
+        }
+        System.out.println();
     }
 }
