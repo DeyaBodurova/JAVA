f28f7e0a0213c29dda9d3039c80b32c9ecc4a551
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 18:13:44 +0300

    wip

--- src/restaurant/ColdBeverage.java
+++ src/restaurant/ColdBeverage.java
@@ -3,7 +3,9 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class ColdBeverage extends Beverages{
-
+    private String name;
+    private BigDecimal price;
+    private double milliliters;
 
     public ColdBeverage(String name, BigDecimal price, double milliliters) {
         super(name, price, milliliters);
--- src/restaurant/HotBeverage.java
+++ src/restaurant/HotBeverage.java
@@ -3,7 +3,9 @@ package restaurant;
 import java.math.BigDecimal;
 
 public class HotBeverage extends Beverages{
-
+    private String name;
+    private BigDecimal price;
+    private double milliliters;
 
     public HotBeverage(String name, BigDecimal price, double milliliters) {
         super(name, price, milliliters);
