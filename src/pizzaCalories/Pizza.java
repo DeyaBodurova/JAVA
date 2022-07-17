c693183e2321bae11b3832c32f29afcf65018ce7
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 08:34:33 +0300

    wip

--- src/pizzaCalories/Pizza.java
+++ src/pizzaCalories/Pizza.java
@@ -4,28 +4,30 @@ import java.util.List;
 
 public class Pizza {
     private String name;
-    private Dough dought;
+    private Dough dough;
     private List<Topping> toppings;
 
     public Pizza(String name, int numberOfToppings) {
         setName(name);
+    }
 
+    public String getName() {
+        return name;
     }
 
     private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
     }
 
     private void setToppings(int toppings) {
 
-    }
-
-    public void setDough(Dough dough) {
-
-    }
-
-    public String getName() {
-        this.name = name;
     }
 
     public void addTopping(Topping topping) {
