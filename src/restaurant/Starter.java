3e05dfc8cfec78a0be36d207787d6f4b163e23a4
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 18:17:35 +0300

    wip

--- src/restaurant/Starter.java
+++ src/restaurant/Starter.java
@@ -2,12 +2,12 @@ package restaurant;
 
 import java.math.BigDecimal;
 
-public class MainDish extends Food{
+public class Starter extends Food {
     private String name;
     private BigDecimal price;
     private double grams;
 
-    public MainDish(String name, BigDecimal price, double grams) {
+    public Starter(String name, BigDecimal price, double grams) {
         super(name, price, grams);
     }
 }
