a39b1335281a60ea69ce67557a9bb2e254932c85
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 14:42:45 +0300

    wip

--- src/footballTeamGenerator/Main.java
+++ src/footballTeamGenerator/Main.java
@@ -8,20 +8,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        Map<String,Team> teams = new LinkedHashMap<>();
+        Map<String, Team> teams = new LinkedHashMap<>();
         while (!input.equalsIgnoreCase("END")) {
             String[] data = input.split(";");
-            String command = data[0].toLowerCase();
-            switch (data[0]) {
+            String commandName = data[0].toLowerCase();
+            String teamName = data[1];
+            switch (commandName) {
                 case "team":
-                    Team team = new Team(data[1]);
+                    Team team = new Team(teamName);
                     break;
                 case "add":
-                    String currentTeamName = data[1];
-                    if(team.getName().equalsIgnoreCase(currentTeamName)) {
-                        Team team = new Team()
+                    if (!teams.containsKey(teamName)) {
+                        throw new IllegalArgumentException(String.format("Team %s does not exist.", teamName));
                     } else {
-                        throw new IllegalArgumentException(String.format("Team %s does not exist.", currentTeamName));
+                        Player player = new Player(data[2], Integer.parseInt(data[3]), Integer.parseInt(data[4]),
+                                Integer.parseInt(data[5]), Integer.parseInt(data[6]), Integer.parseInt(data[7]));
+                        teams.get(teamName).addPlayer(player);
                     }
                     break;
             }
