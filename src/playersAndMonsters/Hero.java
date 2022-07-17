3aaac5cc9954a1465b0ed8002d659d526323aa99
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 17:11:07 +0300

    wip

--- src/playersAndMonsters/Hero.java
+++ src/playersAndMonsters/Hero.java
@@ -1,4 +1,27 @@
 package playersAndMonsters;
 
 public class Hero {
+    private String username;
+    private int level;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Hero(String username, int level) {
+        this.username = username;
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Type: %s Username: %s Level: %s",
+                this.getClass().getName(),
+                this.getUsername(),
+                this.getLevel());
+    }
 }
