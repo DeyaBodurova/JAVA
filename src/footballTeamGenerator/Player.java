64f1febd5c62c0dfcb9e6a1662b4a05f271e0e07
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 12:04:42 +0300

    wip

--- src/footballTeamGenerator/Player.java
+++ src/footballTeamGenerator/Player.java
@@ -1,5 +1,46 @@
 package footballTeamGenerator;
 
 public class Player {
+    private String name;
+    private int endurance;
+    private int sprint;
+    private int dribble;
+    private int passing;
+    private int shooting;
 
+    public Player(String name, int endurance, int sprint, int dribble, int passing, int shooting) {
+
+    }
+
+    private void setName(String name) {
+
+    }
+
+    public String getName() {
+        this.name = name;
+    }
+
+    private void setEndurance(int endurance) {
+
+    }
+
+    private void setSprint(int sprint) {
+
+    }
+
+    private void setDribble(int dribble) {
+
+    }
+
+    private void setPassing(int passing) {
+
+    }
+
+    private void setShooting(int shooting) {
+
+    }
+
+    public double overallSkillLevel() {
+
+    }
 }
