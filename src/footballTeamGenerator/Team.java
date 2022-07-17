03b6906e36ba05946f2920ccb75c30db55bc0a90
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 11:58:53 +0300

    wip

---
+++ src/footballTeamGenerator/Team.java
@@ -0,0 +1,18 @@
+package footballTeamGenerator;
+
+public class Team {
+    private String name;
+    private List<Player> players;
+
++ Team (String)
+
+- setName(String) : void
+
++ getName(): String
+
++ addPlayer(Player) : void
+
++ removePlayer(String) : void
+
++ getRating() : double
+}
