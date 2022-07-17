4230c945c1c34167c305f8b9d4c623bc04835564
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 08:20:29 +0300

    wip

--- src/pizzaCalories/Dough.java
+++ src/pizzaCalories/Dough.java
@@ -1,4 +1,31 @@
 package pizzaCalories;
 
 public class Dough {
+    private String flourType;
+    private String bakingTechnique;
+    private double weight;
+
+    public Dough(String flourType, String bakingTechnique, double weight) {
+        setFlourType(flourType);
+        setBakingTechnique(bakingTechnique);
+        setWeight(weight);
+    }
+
+    private void setFlourType(String flourType) {
+        this.flourType = flourType;
+    }
+
+    private void setBakingTechnique(String bakingTechnique) {
+        this.bakingTechnique = bakingTechnique;
+    }
+
+    private void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+
+
++
+
+    calculateCalories():double
 }
