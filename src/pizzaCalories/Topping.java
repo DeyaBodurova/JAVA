58a4028aac9d35db395a5f3c74d10d5dd9c5f13f
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 08:24:20 +0300

    wip

--- src/pizzaCalories/Topping.java
+++ src/pizzaCalories/Topping.java
@@ -1,4 +1,23 @@
 package pizzaCalories;
 
 public class Topping {
+    private String toppingType;
+    private double weight;
+
+    public Topping(String toppingType, double weight) {
+        setToppingType(toppingType);
+        setWeight(weight);
+    }
+
+    private void setToppingType(String toppingType) {
+        this.toppingType = toppingType;
+    }
+
+    private void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double calculateCalories() {
+
+    }
 }
