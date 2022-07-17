eb5db1b1b424c72b5e767bd3d7fd6cd12edf7753
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 10:05:19 +0300

    wip

--- src/pizzaCalories/Topping.java
+++ src/pizzaCalories/Topping.java
@@ -18,6 +18,22 @@ public class Topping {
     }
 
     public double calculateCalories() {
+        return (2 * weight) * getToppingTypeModifier(toppingType);
+    }
 
+    private double getToppingTypeModifier(String toppingType) {
+        toppingType = toppingType.toLowerCase();
+        switch (toppingType) {
+            case "meat":
+                return 1.2;
+            case "veggies":
+                return 0.8;
+            case "cheese":
+                return 1.1;
+            case "sauce":
+                return 0.9;
+            default:
+                return 0;
+        }
     }
 }
