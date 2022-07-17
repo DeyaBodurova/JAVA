9d75faabb66d93761e00649a18f49bff10b9c48b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-17 07:19:38 +0300

    wip

--- src/OOP/test/Main.java
+++ src/OOP/test/Main.java
@@ -1,5 +1,10 @@
 package OOP.test;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- src/OOP/test/Person.java
+++ src/OOP/test/Person.java
@@ -1,5 +1,7 @@
 package OOP.test;
 
+import java.util.stream.Collectors;
+
 public class Person {
     private String name;
     private double money;
