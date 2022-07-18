c340d343634e84731472f02ede36ead1edf48221
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-18 21:47:05 +0300

    wip

--- src/person/Main.java
+++ src/person/Main.java
@@ -13,16 +13,12 @@ public class Main {
             String type = data[0];
             switch (type) {
                 case "Citizen":
-                    String[] dateData = data[4].split("/");
-                    int yearOfBirth = Integer.parseInt(dateData[2]);
                     Citizen citizen = new Citizen(data[1], Integer.parseInt(data[2]), data[3], data[4]);
                     creatures.add(citizen);
                     break;
                 case "Pet":
-                    String[] dateDataOfPet = data[2].split("/");
-                    int yearOfBirthOfPet = Integer.parseInt(dateDataOfPet[2]);
                     Pet pet = new Pet(data[1], data[2]);
-                    creaturesYearsOfBirth.put(yearOfBirthOfPet, pet);
+                    creatures.add(pet);
                     break;
                 case "Robot":
                     Robot robot = new Robot(data[1], data[2]);
@@ -30,10 +26,12 @@ public class Main {
             }
             input = sc.nextLine();
         }
-        int year = Integer.parseInt(sc.nextLine());
-        if (creaturesYearsOfBirth.containsKey(year)) {
-            creaturesYearsOfBirth.entrySet().removeIf(c -> !c.getKey().equals(year));
-            creaturesYearsOfBirth.forEach(c -> c.);
+        String year = sc.nextLine();
+        for (Birthable birthable : creatures) {
+            if (birthable.getBirthDate().endsWith(year)) {
+                System.out.println(birthable.getBirthDate());
+            }
+
         }
 
     }
