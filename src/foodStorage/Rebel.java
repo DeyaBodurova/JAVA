ef6b30ff839daee04381df1f695082c30253a8c5
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-19 07:15:43 +0300

    wip

---
+++ src/foodStorage/Rebel.java
@@ -0,0 +1,22 @@
+package foodStorage;
+
+public class Rebel {
+    private String name;
+    private  int age;
+    private String group;
+    private int food;
+
+public Rebel(String name, int age, String group) {
+
+}
+
++ getName(): String
+
++ getAge(): int
+
++ getGroup(): String
+
++ getFood(): int
+
++ buyFood(): void
+}
