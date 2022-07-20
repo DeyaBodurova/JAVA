61c68f13d31d559aec4054374adc732d615afedd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-20 06:13:22 +0300

    wip

--- src/foodShortage/Main.java
+++ src/foodShortage/Main.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int peopleCount = Integer.parseInt(sc.nextLine());
+        while (peopleCount>0) {
+            peopleCount--;
 
+        }
 
     }
 }
