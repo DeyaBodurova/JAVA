7a1af385d15d85bc05418057e40532cb0f89cf41
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-21 06:39:07 +0300

    wip

--- src/foodShortage/Main.java
+++ src/foodShortage/Main.java
@@ -1,11 +1,14 @@
 package foodShortage;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int peopleCount = Integer.parseInt(sc.nextLine());
+        Map<String,Buyer> buyer = new LinkedHashMap<>();
         while (peopleCount > 0) {
             peopleCount--;
             String[] data = sc.nextLine().split("\\s+");
