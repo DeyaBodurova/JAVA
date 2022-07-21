cdf3777f8b6188d35076625be34cee3f54e90310
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-21 06:36:53 +0300

    wip

--- src/foodShortage/Main.java
+++ src/foodShortage/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int peopleCount = Integer.parseInt(sc.nextLine());
-        while (peopleCount>0) {
+        while (peopleCount > 0) {
             peopleCount--;
             String[] data = sc.nextLine().split("\\s+");
-            if(data.length == 4) {
+            if (data.length == 4) {
 
             } else {
 
