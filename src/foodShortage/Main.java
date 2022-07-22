72d6d632f4ccf7346c2f330f0f7a6fea3c60be45
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-22 06:56:20 +0300

    wip

--- src/foodShortage/Main.java
+++ src/foodShortage/Main.java
@@ -27,6 +27,7 @@ public class Main {
                 return;
             } else {
               Buyer  buyer = buyers.get(input);
+              buyer.getFood();
             }
             input = sc.nextLine();
         }
