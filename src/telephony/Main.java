d25b93440ebc205ce4b67fd733c06e9225c765cd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-23 06:33:22 +0300

    wip

--- src/telephony/Main.java
+++ src/telephony/Main.java
@@ -1,20 +1,22 @@
 package telephony;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] numbers = sc.nextLine().split("\\s+");
-        String[] sites = sc.nextLine().split("\\s+");
-
+        List<String> numbers = Arrays.stream(sc.nextLine().split("\\s+")).toList();
+        List<String> sites = Arrays.stream(sc.nextLine().split("\\s+")).toList();
+        Smartphone smartphone = new Smartphone(numbers,sites);
         for (String number : numbers) {
             System.out.println("Calling... " + number);
         }
         for (int i = 0; i < sites.length; i++) {
             if (sites[i].contains()) {
             }
-            System.out.println("Browsing: " + site + "!");
+            System.out.println("Browsing: " +  + "!");
         }
     }
 }
