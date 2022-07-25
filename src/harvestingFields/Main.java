ea059bea8af5a05a587497107ded9635ebde60bd
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-07-25 07:06:51 +0300

    wip

--- src/harvestingFields/Main.java
+++ src/harvestingFields/Main.java
@@ -6,4 +6,6 @@ import java.util.Scanner;
 public class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Field[] RICH_SOIL_LAND_FIELDS = RichSoilLand.class.getDeclaredFields();
+    private static final String END_COMMAND = "HARVEST";
+
 }
