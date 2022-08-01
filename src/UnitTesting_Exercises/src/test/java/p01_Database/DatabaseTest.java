352ead3cb8ca282fa2499ab469be300b2ea456f6
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-01 22:30:13 +0300

    wip unit tests

--- src/UnitTesting_Exercises/src/test/java/p01_Database/DatabaseTest.java
+++ src/UnitTesting_Exercises/src/test/java/p01_Database/DatabaseTest.java
@@ -2,12 +2,25 @@ package p01_Database;
 
 import org.junit.Test;
 
+import javax.naming.OperationNotSupportedException;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseTest {
 
 
     @Test
-    public void createDatabase() {
+    public void createDatabase() throws OperationNotSupportedException {
+        Integer [] numbers = {-1, 0, 5, 42, 69, 7};
+        Database database = new Database(numbers);
+        Integer[] dbElements = database.getElements();
+        assertEquals(numbers.length, dbElements.length);
 
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(numbers[i],dbElements[i]);
+        }
+
+       // assertEquals(numbers,dbElements);
     }
 
     @Test
