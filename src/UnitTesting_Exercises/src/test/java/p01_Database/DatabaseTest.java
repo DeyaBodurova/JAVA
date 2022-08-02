9a65de0e68bf9cb23bf3e3bd0ceb33df9c86550a
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-08-02 22:13:02 +0300

    wip unit tests

--- src/UnitTesting_Exercises/src/test/java/p01_Database/DatabaseTest.java
+++ src/UnitTesting_Exercises/src/test/java/p01_Database/DatabaseTest.java
@@ -30,6 +30,7 @@ public class DatabaseTest {
     @Test(expected = OperationNotSupportedException.class)
     public void constructorWithMoreThan16Elements() throws OperationNotSupportedException {
         new Database(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17);
-
     }
+
+    
 }
